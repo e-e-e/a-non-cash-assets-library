f764793d035af6d72f2ef55778ddc9eac3c6da45
e-e-e <e-e-e@users.noreply.github.com>
2016-09-08 16:02:36 +1000

    added hacky match email alert for when new match is made.

--- models/matches.js
+++ models/matches.js
@@ -12,7 +12,19 @@ const transactions = require('../transactions/');
 class Matches {
 
 	static add (have_id, need_id) {
-		return db.query(sql.insert.match,[have_id,need_id]);
+		return db.query(sql.insert.match,[have_id,need_id])
+						.tap(() => 
+							Q.all([
+								db.query('SELECT users.name AS name, users.email as email, t.name as have FROM users INNER JOIN haves USING (user_id) INNER JOIN things t USING (thing_id) WHERE haves.have_id = $1',[have_id]),
+								db.query('SELECT users.name AS name FROM users INNER JOIN needs USING (user_id) WHERE needs.need_id = $1',[need_id])
+								])
+							.spread( (have, need) => {
+								let h = have.rows[0];
+								let n = need.rows[0];
+								return transactions.match(h.email,{have_name: h.name, need_name:n.name });
+							})
+							.catch(err => console.log(err))
+						);
 	}
 
 	static all() {
